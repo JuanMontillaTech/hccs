04e95bf6d606c4096c9d568e17a411da2642dea2
spinales <saul021002@gmail.com>
2022-05-20 22:21:54 -0400

    added Statement income model

---
+++ ERP.Model/Dtos/StatementIncomeDto.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ERP.Domain.Dtos {
+
+    public class StatementIncomeDto {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public decimal Total { get; set; }
+        public string Type { get; set; }
+    }
+
+    public class StatementIncomeGlobalDto {
+        public decimal Amount { get; set; }
+        public List<StatementIncomeDto> StatementIncomes { get; set; }
+    }
+}
