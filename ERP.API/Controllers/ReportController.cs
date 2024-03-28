6677f0512cd909e117baadf87e699fe7b9057e91
Juan Ramon Montilla Sanchez <ing.juan.montilla@gmail.com>
2024-03-28 17:35:11 -0400

    esto no estaba apuntando a el repositorio de sistema

--- ERP.API/Controllers/ReportController.cs
+++ ERP.API/Controllers/ReportController.cs
@@ -25,9 +25,9 @@ namespace ERP.API.Controllers
         private readonly ICurrentUser currentUser;
         private readonly IGenericRepository<PrintingForm> _repPrintingForm;
         private readonly IGenericRepository<ReportQuery> _repPrintingTemplate;
-        private readonly IGenericRepository<Formfields> _repFormfields;
+        private readonly ISysRepository<Formfields> _repFormfields;
         public ReportController(IDirectSql repDynamic, ICurrentUser currentUser, IGenericRepository<PrintingForm> repPrintingForm, 
-            IGenericRepository<ReportQuery> repPrintingTemplate, IGenericRepository<Formfields> repFormfields, IConfiguration config)
+            IGenericRepository<ReportQuery> repPrintingTemplate, ISysRepository<Formfields> repFormfields, IConfiguration config)
         {
             RepDynamic = repDynamic;
             this.currentUser = currentUser;
@@ -43,52 +43,60 @@ namespace ERP.API.Controllers
         [HttpGet("GetById")]
         public async Task<IActionResult> GetById([FromQuery] Guid id, string Data)
         {
-            var PrintForm = await _repPrintingForm.Find(x => x.FormId == id).FirstOrDefaultAsync();
-            dynamic data = null;
-            if (!string.IsNullOrEmpty(Data))
+            try
             {
-              data = JsonConvert.DeserializeObject(Data);
-
-
-            }
-            string conection = "";
-            if (PrintForm != null)
-            {
-               var QueryForm = await _repPrintingTemplate.GetById(PrintForm.PrintingTemplateId);
-               string sqlSelet = QueryForm.Query;
-                if (QueryForm != null)
+                var PrintForm = await _repPrintingForm.Find(x => x.FormId == id).FirstOrDefaultAsync();
+                dynamic data = null;
+                if (!string.IsNullOrEmpty(Data))
                 {
+                    data = JsonConvert.DeserializeObject(Data);
 
-                    List<ReportParametersDto> _params = new  List<ReportParametersDto>();
-                    var Fields = await _repFormfields.Find(x => x.FormId == id && x.IsActive ==true).ToListAsync();
-                    if (Fields.Count >0)
+
+                }
+                string conection = "";
+                if (PrintForm != null)
+                {
+                    var QueryForm = await _repPrintingTemplate.GetById(PrintForm.PrintingTemplateId);
+                    string sqlSelet = QueryForm.Query;
+                    if (QueryForm != null)
                     {
-                      
-                        foreach (var Field in Fields)
+
+                        List<ReportParametersDto> _params = new  List<ReportParametersDto>();
+                        var Fields = await _repFormfields.Find(x => x.FormId == id && x.IsActive ==true).ToListAsync();
+                        if (Fields.Count >0)
                         {
-                            var param = new ReportParametersDto();
-                            param.paramName = Field.Field;
-                            param.paramValue = data[Field.Field];
-                            _params.Add(param);
+                      
+                            foreach (var Field in Fields)
+                            {
+                                var param = new ReportParametersDto();
+                                param.paramName = Field.Field;
+                                param.paramValue = data[Field.Field];
+                                _params.Add(param);
 
 
+                            }
                         }
-                    }
 
                        
 
                   
 
-                    conection = _config.GetConnectionString("DefaultConnection");
+                        conection = _config.GetConnectionString("DefaultConnection");
 
-                    conection = conection.Replace("DbName", currentUser.DataBaseName());
+                        conection = conection.Replace("DbName", currentUser.DataBaseName());
 
-                    var Result = await RepDynamic.QueryDynamic(sqlSelet, _params, conection);
-                    return Ok(Result<dynamic>.Success(Result, MessageCodes.AllSuccessfully()));
+                        var Result = await RepDynamic.QueryDynamic(sqlSelet, _params, conection);
+                        return Ok(Result<dynamic>.Success(Result, MessageCodes.AllSuccessfully()));
+                    }
+
                 }
-
+                return Ok(Result<PrintingFormDto>.Fail("Error al insentar", MessageCodes.BabData()));
             }
-            return Ok(Result<PrintingFormDto>.Fail("Error al insentar", MessageCodes.BabData()));
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return Ok(Result<PrintingFormDto>.Fail("Error al insentar", MessageCodes.BabData(),e.Message));
+            }
 
         }
 
